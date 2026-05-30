3bc73f5e1a78944cefef047aacd3013bc5bc1d48
Fiteh Tesfaye <fitehtesfaye@gmail.com>
2026-05-30 03:15:19 -0700

    update backend cors

--- backend/index.js
+++ backend/index.js
@@ -6,7 +6,13 @@ import cors from "cors";
 import { errorHandler } from "./src/middleware/error-handler.js";
 
 const app = express();
-app.use(cors());
+app.use(
+  cors({
+    origin: "*",
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  }),
+);
 app.use(express.json());
 
 app.use("/api", mainRouter);
