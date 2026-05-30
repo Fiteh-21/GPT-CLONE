58e89f6272d1d1cd87893236ab21058ecfa06bfc
Fiteh Tesfaye <fitehtesfaye@gmail.com>
2026-05-30 02:03:40 -0700

    updated db.config file

--- backend/db/db.config.js
+++ backend/db/db.config.js
@@ -1,6 +1,6 @@
 import mysql from "mysql2/promise";
 
-const urlDB = `mysql://${process.env.MYSQLSUSER}:${process.env.MYSQLPASSWORD}@${process.env.MYSQLHOST}:${process.env.MYSQLPORT}}}/${process.enMYSQLDATABASE}`;
+const urlDB = `mysql://${process.env.MYSQLSUSER}:${process.env.MYSQLPASSWORD}@${process.env.MYSQLHOST}:${process.env.MYSQLPORT}/${process.enMYSQLDATABASE}`;
 
 const db = mysql.createPool(urlDB);
 
